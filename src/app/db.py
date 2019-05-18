14d492773feddc7cc320b5820dbd36d7f54777cc
Stuart George <stuart@accentdesign.co.uk>
2019-05-18 12:55:10 +0100

    Added a way to see all running queries

--- src/app/db.py
+++ src/app/db.py
@@ -5,5 +5,9 @@ from app import settings
 # setup database url
 db = Database(settings.DATABASE_URL)
 
+# print all running queries to the console
+# see https://docs.sqlalchemy.org/en/13/core/engines.html
+# db.engine.echo = True
+
 # import project and external tables
 from starlette_auth import tables  # noqa isort:skip
