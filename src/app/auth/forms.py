61d6b2d6532175e3f163a595d08aaa83aa47e45d
Stuart George <stuart@accentdesign.co.uk>
2020-04-02 10:56:49 +0100

    use the new admin widgets

--- src/app/auth/forms.py
+++ src/app/auth/forms.py
@@ -1,3 +1,4 @@
+from starlette_admin.forms import widgets
 from starlette_auth.tables import Scope, User
 from wtforms import fields, validators
 from wtforms_alchemy.fields import QuerySelectMultipleField
@@ -21,18 +22,24 @@ class UserBaseForm(ModelForm):
 
 
 class UserCreateForm(UserBaseForm):
-    password = fields.PasswordField(validators=[validators.InputRequired()])
+    password = fields.PasswordField(
+        validators=[validators.InputRequired()], widget=widgets.PasswordInput()
+    )
     confirm_password = fields.PasswordField(
         validators=[
             validators.InputRequired(),
             validators.EqualTo("password", message="The passwords do not match."),
-        ]
+        ],
+        widget=widgets.PasswordInput(),
     )
 
 
 class UserUpdateForm(UserBaseForm):
-    scopes = QuerySelectMultipleField(query_factory=all_scopes)
+    scopes = QuerySelectMultipleField(
+        query_factory=all_scopes, widget=widgets.HorizontalSelect()
+    )
 
     class Meta:
         model = User
         only = ["first_name", "last_name", "email", "is_active"]
+        field_args = {"is_active": {"widget": widgets.CheckboxInput()}}
